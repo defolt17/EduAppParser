0ea54e9b2d16713846e41433c4e46e868e585a99
= <=>
2019-10-23 18:47:02 +0300

    Oct-23-18:47:02

--- parse.go
+++ parse.go
@@ -21,19 +21,8 @@ var (
 	loginWait  = time.Millisecond * 1400
 )
 
-// Need to use it somehow
-func try(f func() error) (err error) {
-	for i := 0; ; i++ {
-		err = f()
-		if err == nil {
-			return
-		}
-		if i >= (attempts - 1) {
-			break
-		}
-		time.Sleep(time.Millisecond * 100)
-	}
-	return fmt.Errorf("after %d attempts, last error: %s", attempts, err)
+type Class struct {
+	name, date, weekday, auditory string
 }
 
 func _checkBasic(err error) {
@@ -67,28 +56,31 @@ func loginMain(dr selenium.WebDriver) {
 	time.Sleep(time.Millisecond * 1000)
 }
 
-// TEMP FUNCTION NOT DONE YET
+// Kinda works now
 // Get list of upcoming classes (name, date, time, cabinet)
 func getUpcommingClasses(dr selenium.WebDriver) {
 	loadPage(normalWait, dr, "https://my.informatics.ru/pupil/root")
 	upcommingClasses, err := dr.FindElements(selenium.ByXPATH, "//div[@class='clearfix clickable nowrap']")
 	_checkBasic(err)
-	upcommingClassesList := make([]string, len(upcommingClasses))
+	upcommingClassesList := make([]Class, len(upcommingClasses))
 
 	for i, elem := range upcommingClasses {
-		date, _ := elem.FindElement(selenium.ByXPATH, "//div[@class='top-line']")
-		weekday, _ := elem.FindElement(selenium.ByXPATH, "//span[@class='small text-muted']")
-		auditory, _ := elem.FindElement(selenium.ByXPATH, "//span[@class='small']")
-		courseName, _ := elem.FindElement(selenium.ByXPATH, "//div[@class='col-xs-12 col-sm-6 col-xs-no-padding']")
+		date, _ := elem.FindElement(selenium.ByXPATH, ".//div[@class='top-line']")
+		weekday, _ := elem.FindElement(selenium.ByXPATH, ".//span[@class='small text-muted']")
+		auditory, _ := elem.FindElement(selenium.ByXPATH, ".//span[@class='small']")
+		courseName, _ := elem.FindElement(selenium.ByXPATH, ".//div[@class='col-xs-12 col-sm-6 col-xs-no-padding']")
 
-		dateText, _ := date.Text()
-		weekdayText, _ := weekday.Text()
-		auditoryText, _ := auditory.Text()
-		courseNameText, _ := courseName.Text()
+		upcommingClassesList[i].name, _ = courseName.Text()
+		upcommingClassesList[i].date, _ = date.Text()
+		upcommingClassesList[i].weekday, _ = weekday.Text()
+		upcommingClassesList[i].auditory, _ = auditory.Text()
 
-		upcommingClassesList[i] = courseNameText + dateText + weekdayText + auditoryText
 	}
-	fmt.Println(upcommingClassesList)
+
+	for _, elem := range upcommingClassesList {
+		fmt.Println(elem.name + " " + elem.date + ", " +
+			elem.weekday + " " + elem.auditory)
+	}
 }
 
 func main() {
